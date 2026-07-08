09a30169c1f53dbc3bf70d6e49445df4525251a4
Imesh29 <ridrajakaruna29@gmail.com>
2026-07-09 02:13:50 +0530

    create roleMiddleware

---
+++ backend/middleware/roleMiddleware.js
@@ -0,0 +1,21 @@
+import { errorResponse } from "../utils/response";
+
+const authorize = (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      return errorResponse(res, 401, "Authentication is required");
+    }
+
+    if (!allowedRoles.includes(req.user.role)) {
+      return errorResponse(
+        res,
+        403,
+        "You do not have permission to access this resource",
+      );
+    }
+
+    next();
+  };
+};
+
+export default authorize;
